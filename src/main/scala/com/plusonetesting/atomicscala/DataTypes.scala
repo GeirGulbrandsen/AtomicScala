9fd15c8521120b8d14cbebf9c3802f399b961a0f
Geir Gulbrandsen <dobbelg@gmail.com>
2018-06-07 21:45:23 +0100

    Minor styling fix

--- src/main/scala/com/plusonetesting/atomicscala/DataTypes.scala
+++ src/main/scala/com/plusonetesting/atomicscala/DataTypes.scala
@@ -2,31 +2,31 @@ package com.plusonetesting.atomicscala
 
 object DataTypes extends App {
 
-  val int:Int = 5
+  val int: Int = 5
   println(int)
 
-  val ABC1234:String = "ABC1234"
+  val ABC1234: String = "ABC1234"
   println(ABC1234)
 
-  val double:Double = 5.4
+  val double: Double = 5.4
   println(double)
 
-  val boolean:Boolean = true
+  val boolean: Boolean = true
   println(boolean)
 
-  val multiLine:String =
+  val multiLine: String =
     """Ha ha this
       |is suuuuuper fantastic
       |and quite great!!!
     """.stripMargin
   println(multiLine)
 
-//  val stringBoolean:Boolean = "maybe"
+//  val stringBoolean: Boolean = "maybe"
 //  println(stringBoolean)
 
-//  val doubleInInt:Int = 15.4
+//  val doubleInInt: Int = 15.4
 //  println(doubleInInt)
 
-  val intInDouble:Double = 15
+  val intInDouble: Double = 15
   println(intInDouble)
 }
