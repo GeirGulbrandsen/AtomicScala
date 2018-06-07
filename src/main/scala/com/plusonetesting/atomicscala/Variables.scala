c70457d73d4badfc55373541a59c45531121634f
Geir Gulbrandsen <dobbelg@gmail.com>
2018-06-07 21:54:52 +0100

    Completed Variables

---
+++ src/main/scala/com/plusonetesting/atomicscala/Variables.scala
@@ -0,0 +1,21 @@
+package com.plusonetesting.atomicscala
+
+object Variables extends App {
+
+  var integer: Int = 5
+  integer = 10
+
+  var v1:Int = 5
+  v1 = 10
+  val constantv1:Int = v1
+  println(constantv1)
+
+  v1 = 15
+  println(constantv1)
+
+  var v2:Int = v1
+  println(v2)
+
+  v1 = 20
+  println(v2)
+}
