59207b4829a207aa0639ed21d56024c5560876d7
Geir Gulbrandsen <dobbelg@gmail.com>
2018-06-19 20:33:37 +0100

    Completed Methods Inside Classes atom

--- src/main/scala/com/plusonetesting/atomicscala/E12_MethodsInsideClasses.scala
+++ src/main/scala/com/plusonetesting/atomicscala/E12_MethodsInsideClasses.scala
@@ -10,6 +10,11 @@ object E12_MethodsInsideClasses extends App {
     def lower(): String = {
       "Sails lowered"
     }
+
+    def signal(): String = {
+      val flare = new Flare
+      flare.light()
+    }
   }
 
   class Motorboat {
@@ -20,18 +25,27 @@ object E12_MethodsInsideClasses extends App {
     def off(): String = {
       "Motor off"
     }
+
+    def signal(): String = {
+      val flare = new Flare
+      flare.light()
+    }
   }
 
   val sailboat = new Sailboat
   val r1 = sailboat.raise()
+  val sig1 = sailboat.signal()
   assert(r1 == "Sails raised", "Expected Sails raised, got " + r1)
+  assert(sig1 == "Flare used!", "Expected Flare used!, got " + sig1)
 
   val r2 = sailboat.lower()
   assert(r2 == "Sails lowered", "Expected Sails lowered, got " + r2)
 
   val motorboat = new Motorboat
   val s1 = motorboat.on()
+  val sig2 = motorboat.signal()
   assert(s1 == "Motor on", "Expected Motor on, got " + s1)
+  assert(sig2 == "Flare used!", "Expected Flare used!, got " + sig2)
 
   val s2 = motorboat.off()
   assert(s2 == "Motor off", "Expected Motor off, got " + s2)
@@ -46,5 +60,6 @@ object E12_MethodsInsideClasses extends App {
   val flare = new Flare
   val f1 = flare.light()
   assert(f1 == "Flare used!", "Expected Flare used!, got " + f1)
+
 
 }
