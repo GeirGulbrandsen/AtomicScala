621cce8547e9ffbe1cee2fd1b47f0b02e7f4e8ec
Geir Gulbrandsen <dobbelg@gmail.com>
2018-06-19 09:40:02 +0100

    Completed exercise 1 of Methods Inside Classes atom

--- src/main/scala/com/plusonetesting/atomicscala/E12_MethodsInsideClasses.scala
+++ src/main/scala/com/plusonetesting/atomicscala/E12_MethodsInsideClasses.scala
@@ -3,27 +3,37 @@ package com.plusonetesting.atomicscala
 object E12_MethodsInsideClasses extends App {
 
   class Sailboat {
-    def raise(): Unit = {
-      println("Sails raised")
+    def raise(): String = {
+      "Sails raised"
     }
 
-    def lower(): Unit = {
-      println("Sails lowered")
+    def lower(): String = {
+      "Sails lowered"
     }
   }
 
   class Motorboat {
-    def on(): Unit = {
-      println("Motor on")
+    def on(): String = {
+      "Motor on"
     }
 
-    def off(): Unit = {
-      println("Motor off")
+    def off(): String = {
+      "Motor off"
     }
   }
 
   val sailboat = new Sailboat
   val r1 = sailboat.raise()
   assert(r1 == "Sails raised", "Expected Sails raised, Got " + r1)
+
+  val r2 = sailboat.lower()
+  assert(r2 == "Sails lowered", "Expected Sails lowered, Got " + r2)
+
+  val motorboat = new Motorboat
+  val s1 = motorboat.on()
+  assert(s1 == "Motor on", "Expected Motor on, Got " + s1)
+
+  val s2 = motorboat.off()
+  assert(s2 == "Motor off", "Expected Motor off, Got " + s2)
 
 }
