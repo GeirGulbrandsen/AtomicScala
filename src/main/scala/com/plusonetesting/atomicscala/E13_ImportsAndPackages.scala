660dce512b4ad006905f846ee5fa88881d02ade3
Geir Gulbrandsen <dobbelg@gmail.com>
2018-06-20 21:01:53 +0100

    Complete exercise 1 of Imports & Packages atom

---
+++ src/main/scala/com/plusonetesting/atomicscala/E13_ImportsAndPackages.scala
@@ -0,0 +1,13 @@
+package com.plusonetesting.atomicscala
+
+import com.atomicscala.pythegorean.RightTriangle
+
+object E13_ImportsAndPackages extends App {
+
+  val rt = new RightTriangle
+  println(rt.hypotenuse(3, 4))
+  println(rt.area(3, 4))
+  assert(rt.hypotenuse(3,4) == 5)
+  assert(rt.area(3,4) == 6)
+
+}
