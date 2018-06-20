eb94d5f9e06844a532116d550181f33bb664c009
Geir Gulbrandsen <dobbelg@gmail.com>
2018-06-20 21:17:31 +0100

    Complete exercise 2 of Imports & Packages atom

--- src/main/scala/com/atomicscala/pythegorean/PythegoreanTheorem.scala
+++ src/main/scala/com/atomicscala/pythegorean/PythegoreanTheorem.scala
@@ -1,11 +1,17 @@
 package com.atomicscala.pythegorean
 
 class RightTriangle {
-  def hypotenuse(a:Double, b:Double): Double ={
-    Math.sqrt(a*a + b*b)
+  def hypotenuse(a: Double, b: Double): Double = {
+    Math.sqrt(a * a + b * b)
   }
 
-  def area(a:Double, b:Double): Double = {
-    a*b/2
+  def area(a: Double, b: Double): Double = {
+    a * b / 2
+  }
+}
+
+class EquilateralTriangle {
+  def area(side: Double): Double = {
+    Math.sqrt(3) / 4 * side * side
   }
 }
--- src/main/scala/com/plusonetesting/atomicscala/E13_ImportsAndPackages.scala
+++ src/main/scala/com/plusonetesting/atomicscala/E13_ImportsAndPackages.scala
@@ -1,6 +1,6 @@
 package com.plusonetesting.atomicscala
 
-import com.atomicscala.pythegorean.RightTriangle
+import com.atomicscala.pythegorean.{EquilateralTriangle, RightTriangle}
 
 object E13_ImportsAndPackages extends App {
 
@@ -9,5 +9,9 @@ object E13_ImportsAndPackages extends App {
   println(rt.area(3, 4))
   assert(rt.hypotenuse(3,4) == 5)
   assert(rt.area(3,4) == 6)
+
+  val et = new EquilateralTriangle
+  println(et.area(3))
+  assert(et.area(3) == 3.897114317029974)
 
 }
