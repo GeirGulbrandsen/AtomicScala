62c2d4ce319038f43a00a4bf7d45a331d13ed22f
Geir Gulbrandsen <dobbelg@gmail.com>
2018-06-20 21:26:59 +0100

    Complete Imports & Packages atom

--- src/main/scala/com/plusonetesting/atomicscala/E13_ImportsAndPackages.scala
+++ src/main/scala/com/plusonetesting/atomicscala/E13_ImportsAndPackages.scala
@@ -1,17 +1,27 @@
 package com.plusonetesting.atomicscala
 
 import com.atomicscala.pythegorean._
+import com.plusonetesting.dummypackage._
+//import com.plusonetesting.dummypackage.DummyClass1
+//import com.plusonetesting.dummypackage.{DummyClass1, DummyClass2}
+//import com.plusonetesting.dummypackage.{DummyClass1, DummyClass2, DummyClass3}
 
 object E13_ImportsAndPackages extends App {
 
   val rt = new RightTriangle
   println(rt.hypotenuse(3, 4))
   println(rt.area(3, 4))
-  assert(rt.hypotenuse(3,4) == 5)
-  assert(rt.area(3,4) == 6)
+  assert(rt.hypotenuse(3, 4) == 5)
+  assert(rt.area(3, 4) == 6)
 
   val et = new EquilateralTriangle
   println(et.area(3))
   assert(et.area(3) == 3.897114317029974)
 
+  val d1 = new DummyClass1
+  println(d1)
+  val d2 = new DummyClass2
+  println(d2)
+  val d3 = new DummyClass3
+  println(d3)
 }
---
+++ src/main/scala/com/plusonetesting/dummypackage/DummyClass.scala
@@ -0,0 +1,13 @@
+package com.plusonetesting.dummypackage
+
+class DummyClass1 {
+
+}
+
+class DummyClass2 {
+
+}
+
+class DummyClass3 {
+
+}
